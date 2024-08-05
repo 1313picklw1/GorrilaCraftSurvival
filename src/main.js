fb95b383055479668fdc421bc55e6ee1ba871259
Hecker <1313pickles@gmail.com>
2024-08-05 16:17:28 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
-import * as THREE from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.149.0/build/three.module.js';
+import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.149.0/examples/jsm/loaders/GLTFLoader.js';
+
 
 let scene, camera, renderer;
 let collectibles = [];
